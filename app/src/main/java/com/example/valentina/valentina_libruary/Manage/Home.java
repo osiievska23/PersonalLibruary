ab92779f8d04a012689288872378bc2503529141
Valentina <valentinaosevskaya@gmail.com>
2018-04-09 09:21:22 +0300

    first SwipeMenuList commit

---
+++ app/src/main/java/com/example/valentina/valentina_libruary/Manage/Home.java
@@ -0,0 +1,13 @@
+package com.example.valentina.valentina_libruary.Manage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home);
+    }
+}

8fb1dce0a54eb324dbec13b82979899e442dbe57
Valentina <valentinaosevskaya@gmail.com>
2018-04-10 20:56:10 +0300

    Tabbar commit

---
+++ app/src/main/java/com/example/valentina/valentina_libruary/Fragments/TabBarFragments/BookDescription.java
@@ -0,0 +1,8 @@
+package com.example.valentina.valentina_libruary.Fragments.TabBarFragments;
+
+/**
+ * Created by Valentina on 10.04.2018.
+ */
+
+public class BookDescription {
+}
---
+++ app/src/main/java/com/example/valentina/valentina_libruary/Fragments/TabBarFragments/BookInfo.java
@@ -0,0 +1,18 @@
+package com.example.valentina.valentina_libruary.Fragments.TabBarFragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.valentina.valentina_libruary.R;
+
+
+public class BookInfoFragment extends android.support.v4.app.Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.book_info_fragment, container, )
+    }
+}
